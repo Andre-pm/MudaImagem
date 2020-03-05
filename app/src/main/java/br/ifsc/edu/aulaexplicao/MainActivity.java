afdda56b79b6d281308591396a8f13ab86419fa8
Maria Eduarda <dudasouzalima01@gmail.com>
2020-03-05 11:31:11 -0300

    App

---
+++ app/src/main/java/br/ifsc/edu/aulaexplicao/MainActivity.java
@@ -0,0 +1,24 @@
+package br.ifsc.edu.aulaexplicao;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
+    }
+
+    public void mudaImagem (View view){
+        imageView.setImageResource(R.drawable.porquinho);
+    }
+}
+

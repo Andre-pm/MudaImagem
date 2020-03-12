54eda1bd5a25d73dc26ddbd7f1adf5b0434ef7aa
Maria Eduarda <dudasouzalima01@gmail.com>
2020-03-12 11:13:46 -0300

    Mudando a lista de imagens

--- app/src/main/java/br/ifsc/edu/aulaexplicao/MainActivity.java
+++ app/src/main/java/br/ifsc/edu/aulaexplicao/MainActivity.java
@@ -6,19 +6,42 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity {
     ImageView imageView;
+    List <Integer> lista_imagens;
+    int posicaoImagem ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         imageView = findViewById(R.id.imageView);
-    }
+        lista_imagens = Arrays.asList(
+                R.drawable.cachorro,
+                R.drawable.patinho,
+                R.drawable.gardem,
+                R.drawable.porquinho,
+                R.drawable.happy);
+        posicaoImagem = 0;
+        }
+
 
     public void mudaImagem (View view){
-        imageView.setImageResource(R.drawable.porquinho);
+
+        if (posicaoImagem<lista_imagens.size()-1){
+            imageView.setImageResource(lista_imagens.get(posicaoImagem));
+        }else{
+            posicaoImagem=0;
+            imageView.setImageResource(lista_imagens.get(posicaoImagem));
+
+            }
+        posicaoImagem++;
+        }
+
     }
-}
 
